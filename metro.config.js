6be7e6960d6450b39779cd4d9dd6a50571a5673b
MohammadHamdi11 <mohammadhamdisaid.mh@icloud.com>
2025-05-04 09:48:14 +0300

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -1,32 +1,28 @@
-// metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 const path = require('path');
 
-// Create the default config
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-// Extend the default config
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    extraNodeModules: {
-      // Your aliases
-      '@screens': path.resolve(__dirname, 'screens'),
-      '@services': path.resolve(__dirname, 'services'),
-      '@components': path.resolve(__dirname, 'components'),
-    },
-    sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs'],
-  },
-  transformer: {
-    ...defaultConfig.transformer,
-    minifierConfig: {
-      keep_classnames: true,
-      keep_fnames: true,
-      mangle: {
-        keep_classnames: true,
-        keep_fnames: true,
-      },
-    },
+// Add your customizations
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  '@screens': path.resolve(__dirname, 'screens'),
+  '@services': path.resolve(__dirname, 'services'),
+  '@components': path.resolve(__dirname, 'components'),
+};
+
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  'mjs',
+];
+
+config.transformer.minifierConfig = {
+  keep_classnames: true,
+  keep_fnames: true,
+  mangle: {
+    keep_classnames: true,
+    keep_fnames: true,
   },
 };
+
+module.exports = config;
